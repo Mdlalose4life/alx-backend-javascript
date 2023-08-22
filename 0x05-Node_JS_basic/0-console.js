372bf5999e103a70f2f59b36c941b6a43054360d
Mdlalose4life <blessing1059@gmail.com>
2023-08-22 23:40:20 +0200

    The console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints the name passed to in on STDOUT
+ * 
+ * @param {msg} -  Message to be printed
+ */
+function displayMessage(msg){
+    console.log(msg);
+}
+
+module.exports = displayMessage;
