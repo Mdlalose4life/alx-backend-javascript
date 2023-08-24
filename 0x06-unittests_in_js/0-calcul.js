edd0396637e2fa637c0a8e7a64f960bf7817a57e
Mdlalose4life <blessing1059@gmail.com>
2023-08-24 13:03:43 +0200

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,18 @@
+/**
+ * The function that adds two rounded numbers
+ * @param {number} a - First digit to be rounded and added
+ * @param {number} b - Second digit to be rounded and added
+ * @returns - The sum of rounded first and second digit.
+ */
+
+
+function calculateNumber(a, b) {
+    // Round the fist digit to the nearest interger
+    const rounded_a = Math.round(a)
+    // Round the second digit to the nearest interger
+    const rounded_b = Math.round(b)
+    // Retun the sum of the rounded digits
+    return rounded_a + rounded_b
+}
+// Export calculateNumber so it can be imported to another module
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,52 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+    it('Test for whole numbers', () => {
+        assert.strictEqual(calculateNumber(1.0, 3.0), 4.0);
+    });
+
+    it('Test for fraction in b', () => {
+        assert.strictEqual(calculateNumber(1.0, 0.5), 2.0);
+    });
+
+    it('Test for fraction in a', () => {
+        assert.strictEqual(calculateNumber(0.5, 3.0), 4.0);
+    });
+
+    it('Test for fraction in a and b', () => {
+        assert.strictEqual(calculateNumber(2.5, 3.4), 6.0);
+    });
+
+    it('Test if floats sums to a whole number', () => {
+        assert.strictEqual(calculateNumber(0.5, 0.5), 2.0);
+    });
+
+    it('Test for both floating points', () => {
+        assert.strictEqual(calculateNumber(2.5, 3.5), 7.0);
+    });
+
+    it('Test for negative numbers', () => {
+        assert.strictEqual(calculateNumber(-1.0, 3.0), 2.0);
+    });
+
+    it('Test for both zeros', () => {
+        assert.strictEqual(calculateNumber(0, 0), 0);
+    });
+
+    it('Test for large whole numbers', () => {
+        assert.strictEqual(calculateNumber(100000000.3, 400000000.0), 500000000);
+    });
+
+    it('Test for floats with trailing numbers', () => {
+        assert.strictEqual(calculateNumber(1.4, 3.6), 5.0);
+    });
+
+    it('Test for edge cases', () => {
+        assert.equal(calculateNumber(0.4, 0.6), 1);
+        assert.equal(calculateNumber(0.49, 0.51), 1);
+        assert.equal(calculateNumber(-0.4, -0.6), -1);
+        assert.equal(calculateNumber(-0.49, -0.51), -1);
+        assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+      });
+});
