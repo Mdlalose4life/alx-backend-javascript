58b7c10e1d0a9cf5164e6d913d0f3981cfb18618
Mdlalose4life <blessing1059@gmail.com>
2023-08-26 01:09:10 +0200

    readfiles

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,17 @@
-const fs = require('fs')
+/**
+ * Prompt the user to enter thier names and read their names back
+ * to the user
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-function countStudents(() {
-    fs.readFileSync
-})
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${ name }`)
+  }
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write(`This important software is now closing\n`)
+});
