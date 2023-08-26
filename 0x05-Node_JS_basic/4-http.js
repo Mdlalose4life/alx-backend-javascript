0cb27d1d796eea19153207dfe68ae7664536e37a
Mdlalose4life <blessing1059@gmail.com>
2023-08-26 04:51:15 +0200

    app

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+/**
+ * Callback function that handles incoming HTTP requests.
+ * @param {http.IncomingMessage} req The incoming HTTP request.
+ * @param {http.ServerResponse} res The server response that will be sent back to the client.
+ */
+const handleRequest = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+};
+
+const app = http.createServer(handleRequest);
+
+app.listen(1245);
+
+module.exports = app;
